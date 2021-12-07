b921f39cca3eab7aba710f7ae6876b2e020e2b88
dahvsface <62912096+dahvsface@users.noreply.github.com>
2021-12-06 21:39:49 -0800

    Delete searchplayergame.js

--- public/searchplayergame.js
+++
@@ -1,6 +0,0 @@
-function searchPlayerGame() {
-    //get gamertag
-    var playerGamertag  = document.getElementById('playersGamertag').value
-    //construct the URL and redirect to it
-    window.location = '/playersGames/search/' + encodeURI(playerGamertag)
-}
